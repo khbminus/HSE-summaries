e2102234f631c2ede0b6631d6328bdb971bbc3a1
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2022-03-27 16:48:34 +0300

    made tickets text files

--- src/term2-calculus/make_tickets.sh
+++ src/term2-calculus/make_tickets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env /bin/bash
 mkdir -p ../term2-calc1-tickets
 for i in {01..59} ; do
-    grep -Phzo "%BEGIN TICKET $i([\s\S]+)(?<=%END TICKET $i)" ??-*.tex > "../term2-calc1-tickets/ticket$i.tex"
+    grep -Phazo "%BEGIN TICKET $i([\s\S]+)(?<=%END TICKET $i)" ??-*.tex > "../term2-calc1-tickets/ticket$i.tex"
 done
 
